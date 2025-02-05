615ed05ab8d06745f76341c8dc371b94e47a67ba
condua <phuc.phanphuc2002@hcmut.edu.vn>
2025-02-05 14:39:34 +0700

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,54 @@ function App() {
           <Portfolio />
           <Contact />
         </main>
-        <footer className="py-4 text-center bg-gray-200">
-          © {new Date().getFullYear()} Phan Hoàng Phúc. All rights reserved.
+        <footer className="py-4 text-center bg-gray-200 flex md:flex-row flex-col items-center justify-around w-full">
+          <p className="md:text-xl text-xss">
+            © {new Date().getFullYear()} Phan Hoàng Phúc. All rights reserved.
+          </p>
+          <nav>
+            <ul className="flex justify-center items-center md:gap-x-5 gap-x-3">
+              <li>
+                <a
+                  href="https://github.com/phanhoangphuc"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="md:text-3xl text-2xl text-blue-600 hover:text-gray-800 transition duration-300 ease-in-out"
+                >
+                  <i className="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/phanhoangphuc"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="md:text-3xl text-2xl text-red-600 hover:text-red-800 transition duration-300 ease-in-out"
+                >
+                  <i className="fab fa-youtube"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/phanhoangphuc"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="md:text-3xl text-2xl text-blue-600 hover:text-red-800 transition duration-300 ease-in-out"
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/phanhoangphuc"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-3xl text-gray-800 hover:text-gray-600 transition duration-300 ease-in-out"
+                >
+                  <i className="fab fa-tiktok"></i>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </footer>
       </div>
     </LanguageProvider>
